98890a0140737089398dca762c63fdbf64c5dae4
activemac05 <activemac05@activemc05simac>
2019-02-20 11:49:53 +0530

    created test suite 4

---
+++ Test Suites/TEST SUITE4.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TEST SUITE4</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>1ccfb4f9-4353-4b78-9bd7-f111a1f0445b</testSuiteGuid>
+</TestSuiteEntity>
